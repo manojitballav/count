0ddfa6fc3268f7b45651b0da28b03f8d6ed5f339
Manojit Ballav <manojitocto@gmail.com>
2017-03-02 18:55:48 +0530

    Most common words program

---
+++ most_common.py
@@ -0,0 +1,13 @@
+#Python program to find most nunmber common words in a text file
+
+#impoting the required libraries
+import re
+from collections import Counter
+
+#find words in the specified text and converting them into lowercase letters
+words = re.findall(r'\w+', open('C:\\Users\\mono\\Desktop\\hd.txt').read().lower())
+
+#finding out the most common words
+count = Counter(words).most_common(20)
+print count
+
