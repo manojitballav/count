9ce62d61245420a5d2c02929062521cb12669398
Manojit Ballav <manojitocto@gmail.com>
2017-03-02 18:56:56 +0530

    Update most_common.py

--- most_common.py
+++ most_common.py
@@ -8,6 +8,6 @@ from collections import Counter
 words = re.findall(r'\w+', open('C:\\Users\\mono\\Desktop\\hd.txt').read().lower())
 
 #finding out the most common words
-count = Counter(words).most_common(20)
+count = Counter(words).most_common(20) #change the value of the argument to increase or decrease the number of common words 
 print count
 
